759e8c5d3c39fe68c109f4e60e9b7caf3eab3ba9
charl11e <146877892+charl11e@users.noreply.github.com>
2025-06-16 21:40:08 +0100

    Initial commit

---
+++ src/main/java/io/github/charl11e/videogame_tracker/VideogameTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.github.charl11e.videogame_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideogameTrackerApplication.class, args);
+	}
+
+}

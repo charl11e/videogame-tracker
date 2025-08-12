aea5e362dd3d2c0e55d58be76d5ba8c896d5ba43
charl11e <146877892+charl11e@users.noreply.github.com>
2025-08-12 17:41:03 +0100

    Automatically makes a default user on start if there are no users

--- backend/src/main/java/io/github/charl11e/videogame_tracker/VideogameTrackerApplication.java
+++ backend/src/main/java/io/github/charl11e/videogame_tracker/VideogameTrackerApplication.java
@@ -1,13 +1,29 @@
 package io.github.charl11e.videogame_tracker;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.charl11e.videogame_tracker.model.User;
+import io.github.charl11e.videogame_tracker.repository.UserRepository;
 
 @SpringBootApplication
 public class VideogameTrackerApplication {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(VideogameTrackerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		if (userRepository.count() == 0) {
+			User user = new User();
+			user.setUsername("Default User");
+			userRepository.save(user);
+		}
 	}
 
 }
